f3dbb811e8ae9cd166b1c39fe45f46933418142b
Todd Gray <todd@g3s.net>
2017-10-22 13:32:54 -0700

    Some cleanup and IfOnEdgeBounce

--- ScaredTurtle.py
+++ ScaredTurtle.py
@@ -1,18 +1,17 @@
 
 import Snappy
-
 import random
+
 
 class ScaredTurtle(Snappy.Sprite):
 
     def WhenIAmClicked(self):
         self.PenDown()
-        self.Say("Yikes!",0.5)
-        x = random.randrange(-240,240)
-        y = random.randrange(-180,180)
-        print("heading to %d,%d" % (x,y))
-        self.PointTowards((x,y))
-        self.Glide(2,x,y)
+        self.Say("Yikes!", 0.5)
+        x = random.randrange(-240, 240)
+        y = random.randrange(-180, 180)
+        self.PointTowards((x, y))
+        self.Glide(2, x, y)
 
     def WhenIAmMouseEntered(self):
         self.Think("Uh oh...", 1)
@@ -20,8 +19,9 @@ class ScaredTurtle(Snappy.Sprite):
     def When_space_Pressed(self):
         self.Clear()
 
-    def WhenKeyPressed(self,key):
+    def WhenKeyPressed(self, key):
         self.Say("You pressed %s!" % key, 2)
+
 
 Snappy.TheProject.AddSprite(ScaredTurtle())
 Snappy.TraceLevel = Snappy.tracelevel.Info
